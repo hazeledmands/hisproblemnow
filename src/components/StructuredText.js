069de95295eb227abf19c28919e22792b6e63f34
Max Edmands <git@maxedmands.com>
2016-11-22 21:16:24 -0800

    Handle Prismic StructuredText properly

--- src/components/StructuredText.js
+++ src/components/StructuredText.js
@@ -1,21 +1,69 @@
 import React, {PropTypes} from 'react';
+import _ from 'lodash';
 
-const StructuredText = (props) => (
-  <div className='structured-text'>
-    {props.value.map((block, i) => {
-      if (block.type === 'paragraph') {
-        return (
-          <p className='block' key={i}>
-            {block.text.split('\n').map((segment, i) => 
-              <span key={i}>
-                {segment}<br />
-              </span>
-            )}
-          </p>
-        )
-      }
-    })}
-  </div>
-);
+function StructuredText ({value}) {
+  return (
+    <div className='structured-text'>
+      {value.map((block, i) => (
+        <Block key={i} {...block} />
+      ))}
+    </div>
+  );
+};
+
+function Block({type, spans, text}) {
+  let segments = [];
+  let index = 0;
+
+  spans.forEach(function (span) {
+    if(span.start !== index) {
+      segments.push({
+        text: text.slice(index, span.start),
+        type: 'span',
+      });
+    }
+    segments.push({
+      text: text.slice(span.start, span.end),
+      type: span.type,
+      data: span.data,
+    });
+    index = span.end;
+  });
+  if (index < text.length - 1) {
+    segments.push({
+      text: text.slice(index),
+      type: 'span',
+    })
+  }
+  return (
+    <p>
+      {segments.map(function (segment, i) {
+        switch (segment.type) {
+          case 'span':
+            return (<span key={i}><Segment text={segment.text} /></span>)
+          case 'strong':
+            return (<b key={i}><Segment text={segment.text} /></b>)
+          case 'hyperlink':
+            return (<a key={i} href={_.get(segment, 'data.value.url')} target='_blank'><Segment text={segment.text} /></a>)
+          default:
+            return (<tt key={i}>{JSON.stringify(segment)}</tt>)
+        }
+      })}
+    </p>
+  );
+}
+
+function Segment({text}) {
+  const lines = text.split('\n')
+  return (
+    <span>
+      {lines.map((line, i) =>
+        <span key={i}>
+          {line}{i < lines.length - 1 && <br />}
+        </span>
+      )}
+    </span>
+  )
+}
 
 export default StructuredText;
